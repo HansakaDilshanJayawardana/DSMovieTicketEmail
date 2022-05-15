e28ffd03c7046395141bcc0df211086d35713c50
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-16 04:54:21 +0530

    Modified Email Controller Class

--- src/main/java/com/email/dsmovieticketemail/EmailController.java
+++ src/main/java/com/email/dsmovieticketemail/EmailController.java
@@ -1,6 +1,10 @@
 package com.email.dsmovieticketemail;
 
+import com.email.dsmovieticketemail.payload.EmailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +17,17 @@ public class EmailController {
     private EmailAlert emailAlert;
 
     @RequestMapping("/api/v1/notification/send-mail")
-    public void sendMail() throws MessagingException {
-        emailAlert.sendEmail("hansaka345@gmail.com", "Payment Confirmation", "This is a payment confirmation message!!!");
+//    public void sendMail() throws MessagingException {
+//        emailAlert.sendEmail("hansaka345@gmail.com", "Payment Confirmation", "This is a payment confirmation message!!!");
+//    }
+
+    public ResponseEntity<String> sendMail(@RequestBody EmailRequest emailRequest) throws MessagingException {
+        try {
+            emailAlert.sendEmail(emailRequest.getEmail(), emailRequest.getSubject(), emailRequest.getBody());
+            return new ResponseEntity<>("Send Email", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed Email", HttpStatus.FAILED_DEPENDENCY);
+        }
     }
 
 }
